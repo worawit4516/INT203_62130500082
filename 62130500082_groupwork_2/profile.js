0c5aba94b37b0af3f11900478724cfcbad032800
Worawit Makkasane <worawit.4516@mail.kmutt.ac.th>
2021-02-03 10:43:57 +0700

    [update]

---
+++ 62130500082_groupwork_2/profile.js
@@ -0,0 +1,22 @@
+    const app = {
+        data() {
+            return {
+                firstname: 'Worawit',
+                lastname: 'Makkasan',
+                role: 'Database Administrator',
+                article: 50,
+                follower: 1524,
+                rating: '9.8',
+                image: "./images/2.jpg"
+            }
+        },
+        mounted: function() {
+            this.$nextTick(function() {
+                setInterval(() => {
+                    mountedApp.follower += 1
+                }, 1000)
+            })
+        }
+
+    }
+    mountedApp = Vue.createApp(app).mount('#app')
